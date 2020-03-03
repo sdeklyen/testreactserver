4cf1fc2f5e82f1094b3e195f7d6ff173ecdb1e64
Sam DeKlyen <samdeklyen@gmail.com>
2020-03-02 22:16:39 -0500

    hopeful

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.use(express.json());
 app.use(require('./middleware/headers'));
 
 app.use('/cat', cat)
-app.listen(3001, function(){
-    console.log('App is listening on 3001.')
+app.listen(process.env.PORT, function(){
+    console.log(`App is listening on ${process.env.PORT}.`)
 })
